9cf5a6b6f0edcbd59ea650cc853d8a293ac9ce96
Meggan <meggtriplett@gmail.com>
2019-06-18 23:35:47 -0700

    Completed linklist class methods: insert and print

---
+++ Linked List/Linked List/Classes/LinkList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List.Classes
+{
+    class LinkList
+    {
+        public Node Head { get; set; }
+
+        public LinkList(int value)
+        {
+            Node node = new Node(value);
+            Head = node;
+        }
+
+        /// <summary>
+        /// Creates new node and puts it at the front of the link list
+        /// </summary>
+        /// <param name="value"> Value stored in the new node </param>
+        public void Insert(int value)
+        {
+            Node node = new Node(value);
+            node.Next = Head;
+            Head = node;
+        }
+
+        /// <summary>
+        /// Display in console the value of each node in linked list
+        /// </summary>
+        public void Print()
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                Console.Write($"{current.Value} => ");
+                current = current.Next;
+            }
+
+            Console.WriteLine("null");
+
+        }
+    }
+
+}
+
